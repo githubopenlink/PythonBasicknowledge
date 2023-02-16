a609601be1fde3dd9761b45e084473bb1307fe33
githubopenlink <125510718+githubopenlink@users.noreply.github.com>
2023-02-16 19:21:43 +0800

    Add files via upload

---
+++ GUI图形界面/Tkinter库/建立窗口.py
@@ -0,0 +1,11 @@
+import tkinter
+
+root=tkinter.Tk()
+#设置界面标题
+root.title('第一个界面')
+#设置窗口大小 数值是字符串形式 中间是相乘的
+root.geometry('800x600')
+# 更改左上角窗口的的icon图标
+root.iconbitmap('E:\素材\素材\篮球.jpeg')
+#开启主循环，让窗口处于显示状态
+root.mainloop()
